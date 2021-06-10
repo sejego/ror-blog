b58f977191b733ffa91c7c76e1de1f7d1950aff8
Sergei <sejego@taltech.ee>
2021-06-10 16:11:45 +0300

    remove DB change

--- db/migrate/20210609121101_create_articles.rb
+++ db/migrate/20210609121101_create_articles.rb
@@ -2,7 +2,6 @@ class CreateArticles < ActiveRecord::Migration[6.1]
   def change
     create_table :articles do |t|
       t.string :title
-      t.text :short_description
       t.text :body
 
       t.timestamps
